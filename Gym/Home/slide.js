078d58734e4a8475855f060e42d81c0b98ad5db8
Yamani AL_tamimi <yamani5006@gmail.com>
2024-03-18 14:19:41 +0300

    adding new style fot slid page

---
+++ Gym/Home/slide.js
@@ -0,0 +1,27 @@
+const navItems = document.querySelectorAll(".nav-item");
+const page =document.getElementById ("page");
+const page_src=["./home.html","../Adding/RegisterOf_Participant.html","./gallery.html","about.html"]
+ 
+navItems.forEach((navItem, i) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach((item, j) => {
+      item.className = "nav-item";
+      if(j == i){
+    
+         page.src=page_src[i];
+     
+    }
+
+    });
+    navItem.className = "nav-item active";
+     
+    // if(navItem.className= "nav-item active p1"){
+    //     page.src="./home.html";
+    // }else if (navItem.className = "nav-item active p2"){
+    //   page.src="./404.html";
+    // } else {
+    //     page.src="./index.html"
+   
+    
+  });
+});
