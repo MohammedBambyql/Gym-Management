6162c401fdf6d1b424b7d21211cd7da1a32e7a18
Yamani AL_tamimi <yamani5006@gmail.com>
2024-03-27 06:12:41 +0300

    some change from view page and put data with it

--- Gym/javaScript/view-members.js
+++ Gym/javaScript/view-members.js
@@ -1,16 +1,16 @@
-fetch("").then((data) => {
-  return data.json();
-}).then((ObjectData) => {
-  console.log(ObjectData[0].titles);
-  let tableData = "";
-  ObjectData.map((values)=> {
-    tableData += `<tr>
-    <td>`+values.title+`</td>
-    <td>`+values.start_time+`</td>
-    <td>`+values.end_time+`</td>
-    <td>`+values.location+`</td>
-    <td>`+values.description+`</td>
-    </tr>`
-  })
-  document.getElementById("table_body").innerHTML = tableData;
-})
+// fetch("").then((data) => {
+//   return data.json();
+// }).then((ObjectData) => {
+//   console.log(ObjectData[0].titles);
+//   let tableData = "";
+//   ObjectData.map((values)=> {
+//     tableData += `<tr>
+//     <td>`+values.title+`</td>
+//     <td>`+values.start_time+`</td>
+//     <td>`+values.end_time+`</td>
+//     <td>`+values.location+`</td>
+//     <td>`+values.description+`</td>
+//     </tr>`
+//   })
+//   document.getElementById("table_body").innerHTML = tableData;
+// })
--- Gym/views/view-members.js
+++ Gym/views/view-members.js
@@ -1,11 +1,167 @@
-new DataTable('#example', {
-    ajax: 'data/objects.txt',
-    columns: [
-        { data: 'name' },
-        { data: 'position' },
-        { data: 'office' },
-        { data: 'extn' },
-        { data: 'start_date' },
-        { data: 'salary' }
-    ]
-});
+// const getNewCases = async() => {
+//     const response = await fetch('https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=fd80f4ca0a7645b4ad9a7fb57e6ea6dd');
+//     const data = await response.json();
+//     let usa = data.filter(val => {
+//       return val.articles[1] === "Abendzeitung";
+//     });
+//     $('#myTable').DataTable({
+//       data: usa,
+//       bLengthChange: false,
+//       columns: [
+//         { data: 'provinceState', title: 'State' },
+//         { data: 'countryRegion', title: 'Country' },
+//         { data: 'lastUpdate', title: 'Last Update' },
+//         { data: 'confirmed', title: 'Confirmed' },
+//         { data: 'deaths', title: 'Deaths' },
+//         { data: 'recovered', title: 'Recovered' }
+//       ]
+//     });
+//   };
+  
+//   scrollToPager = () => {
+//     var y = $(window).scrollTop();
+//     $('html, body').animate({
+//       scrollTop: y + $('#myTable').height()
+//     })
+//   }
+  
+//   getNewCases();
+
+// var datainfo=[];
+
+// for(i=0;i<3;i++){
+
+// var data={
+//     name: "ali",
+//     email : "yamani@" ,
+//     number:12345,
+//     mes:12324
+
+// }
+// datainfo.push(data);
+// showdata();
+// }
+
+// function showdata(){
+
+// var row="";
+// for(i=0;i<datainfo.length;i++){
+//     row+=`
+//     <tr>
+//     <td>`+datainfo[i].name+`</td>
+//     <td>`+datainfo[i].email+`</td>
+//     <td>`+datainfo[i].number+`</td> 
+//     <td>`+datainfo[i].mes+`</td>
+//     <td><button class="btn btn-danger" onclick=del(`+i+`)>Delete</button></td>
+//     </tr>
+
+//     `;
+// }
+// document.getElementById("tbody").innerHTML=row;
+
+// }
+
+var fullname=document.getElementById("fullname");
+var email=document.getElementById("email");
+var number=document.getElementById("number");
+var age=document.getElementById("age");
+var price=document.getElementById("price");
+var chooseCoach=document.getElementById("select");
+var sendBtn=document.getElementById("sendBtn");
+var tbody=document.getElementById("tbody");
+var type=document.getElementById("type");
+var currentRow=document.getElementById("currentRow");
+
+
+// start 
+
+// array that collate the objects
+
+
+var data=[];
+
+
+
+// sendBtn used to save value and show it in table
+sendBtn.addEventListener("click",function(){
+    var user={
+        namee:fullname.value,
+        email:email.value,
+        phone:number.value,
+        age:age.value,
+        price:price.value
+    };
+    if(type.value==="create"){
+         data.push(user);
+    }
+    else if (type.value === "update"){
+        data[currentRow.value]=user;
+    }
+    showData();
+    reset();
+})
+
+
+// function get the data from api and put the data to data array
+function getdata(){}
+
+
+// function that show the data to table 
+function showData(){
+var row="";
+for(i=0;i<data.length;i++){
+    row+=`
+       <tr>
+       
+       <td>`+data[i].namee+`</td>
+       <td>`+data[i].email+`</td>
+       <td>`+data[i].phone+`</td>
+       <td>`+data[i].age+`</td>
+       <td>`+data[i].price+`</td>
+       <td><button class="btn btn-danger" onclick="deletee(`+i+`)">delete</button></td>
+       <td><button class="btn btn-info" onclick="updatee(`+i+`)">update</button></td>
+       </tr>
+       
+    
+    `;
+}
+document.getElementById("tbody").innerHTML=row;
+}
+
+// function used to delete the object 
+
+function deletee(i){
+    data.splice(i,1);
+    showData();
+};
+// update button will be work when click on
+
+function updatee(i){
+   
+    fullname.value=data[i].namee;
+    email.value=data[i].email;
+    number.value=data[i].phone;
+    age.value=data[i].age;
+    price.value=data[i].price;
+    currentRow.value=i;
+     type.value="update";
+}
+
+function reset(){
+    type.value="create";
+    fullname.value="";
+    email.value="";
+    number.value="";
+    age.value="";
+    price.value="";
+
+}
+
+
+
+
+
+
+
+
+
