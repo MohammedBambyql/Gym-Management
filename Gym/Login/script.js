0d1db4c2f0195a31d1519804703a0b1b94daf8bb
Yamani AL_tamimi <yamani5006@gmail.com>
2024-03-29 01:47:36 +0300

    adding api AJAX code for login page

--- Gym/Login/script.js
+++ Gym/Login/script.js
@@ -1,27 +1,79 @@
 var btn=document.getElementById("ui");
 var User=document.getElementById("User");
 var pass=document.getElementById("pass");
+var FormLogin=document.getElementById("form");
+var errorMes=document.getElementById("error");
 
 
 
 
 
-btn.onclick=function(){
-    if(User.value==123 && pass.value==123){
-        alert("登录成功！")
-       location.href="../Home/index.html";
-    }
+// btn.onclick=function(){
+//     if(User.value==123 && pass.value==123){
+//         alert("登录成功！")
+//        location.href="../Home/slide.html";
+//     }
 
-    else{
-        alert("wrong");
-    }
-}
+//     else{
+//         errorMes.innerHTML="Sorry userName or password was wrong";
+//         errorMes.style.color="red";
+//     }
+// }
   
+FormLogin.addEventListener("submit",async(e)=>{
+e.preventDefault();
+
+//basic input validation (option)
+
+if(!User.value || !pass.value ) {
+   
+    errorMes.innerHTML= "All fields are required.";
+    errorMes.style.color="red";
+    errorMes.style.an
+     return
+}
+
+
+const userName=User.value;
+const password=pass.value;
+
+try{
+const respose=await fetch("url",{method:"post",headers:{
+    "Content-Type":"application/json"},body:JSON.stringify({userName,password})
+
+});
+if(!respose.ok){
+    throw new Error("Api request failed with status"+respose.status);
+
+}
+const data=await respose.json();
+if(data.success){
+    //login  success redirect to home page
+    location.href="../Home/slide.html"; 
+}
+
+else{
+    errorMes.innerHTML="user name or password is Wrong";
+    errorMes.style.color="red";
+
+}
+}
+catch (error){
+console.log("Error :" ,error);
+errorMes.innerHTML="some  thing went wrong ";
+errorMes.style.color="red";
+}
+finally{
+    pass.value ="";
+}
+});
 
 
 
 
-// btn.addEventListener("click",()=>{
+
+
+//ebtn.addEventListener("click",()=>{
 //     if(User=="admin" && pass=="admin"){
 //         alert("done");
 //         window.location.assign("../Home/index.html");
