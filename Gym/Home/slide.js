7366bd7a523ff811cfaa17036aa58dde0d76c6a0
Abo Night <ahmdalqhlany@gmail.com>
2024-04-07 01:24:37 +0300

    subscribe-member fixed

--- Gym/Home/slide.js
+++ Gym/Home/slide.js
@@ -1,27 +1,27 @@
 const navItems = document.querySelectorAll(".nav-item");
 const page =document.getElementById ("page");
-const page_src=["./home.html","../views/newView.html","../views/view.html","about.html"]
- 
+const page_src=["./home.html","../views/view_view.htm","../views/view.html","about.html","../plan/tableOfPlan.htm"]
+
 navItems.forEach((navItem, i) => {
   navItem.addEventListener("click", () => {
     navItems.forEach((item, j) => {
       item.className = "nav-item";
       if(j == i){
     
-         page.src=page_src[i];
-     
+        page.src=page_src[i];
+    
     }
 
     });
     navItem.className = "nav-item active";
-     
+    
     // if(navItem.className= "nav-item active p1"){
     //     page.src="./home.html";
     // }else if (navItem.className = "nav-item active p2"){
     //   page.src="./404.html";
     // } else {
     //     page.src="./index.html"
-   
+  
     
   });
 });
