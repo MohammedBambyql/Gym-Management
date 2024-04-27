7e46ee5805b5c0d7a67302be59aad6ce3e3626ec
MohammedBambyql <mhmdbambyql123@gmail.com>
2024-04-27 12:13:43 -0700

    Add API for (Login) page and some change on login.html file

--- Gym/javaScript/login.js
+++ Gym/javaScript/login.js
@@ -1,84 +1,28 @@
-var btn=document.getElementById("ui");
-var User=document.getElementById("User");
-var pass=document.getElementById("pass");
-var FormLogin=document.getElementById("form");
-var errorMes=document.getElementById("error");
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
+    var data = {
+        userName: username,
+        password: password
+    };
 
-
-
-
-// btn.onclick=function(){
-//     if(User.value==123 && pass.value==123){
-//         alert("登录成功！")
-//        location.href="../Home/slide.html";
-//     }
-
-//     else{
-//         errorMes.innerHTML="Sorry userName or password was wrong";
-//         errorMes.style.color="red";
-//     }
-// }
-  
-FormLogin.addEventListener("submit",async(e)=>{
-e.preventDefault();
-
-//basic input validation (option)
-
-if(!User.value || !pass.value ) {
-   
-    errorMes.innerHTML= "All fields are required.";
-    errorMes.style.color="red";
-    errorMes.style.an
-     return
-}
-
-
-const userName=User.value;
-const password=pass.value;
-
-try{
-const respose=await fetch("url",{method:"post",headers:{
-    "Content-Type":"application/json"},body:JSON.stringify({userName,password})
-
-});
-if(!respose.ok){
-    throw new Error("Api request failed with status"+respose.status);
-
-}
-const data=await respose.json();
-if(data.success){
-    //login  success redirect to home page
-    location.href="../Home/slide.html"; 
-}
-
-else{
-    errorMes.innerHTML="user name or password is Wrong";
-    errorMes.style.color="red";
-
-}
-}
-catch (error){
-console.log("Error :" ,error);
-errorMes.innerHTML="some  thing went wrong ";
-errorMes.style.color="red";
-}
-finally{
-    pass.value ="";
-}
-});
-
-
-
-
-
-
-//ebtn.addEventListener("click",()=>{
-//     if(User=="admin" && pass=="admin"){
-//         alert("done");
-//         window.location.assign("../Home/index.html");
-//     }
-//     else{
-//         alert("wrong");
-//     } 
-// })
+    fetch('https://localhost:7209/api/Authentication/Login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.success) {
+            window.location.href = '/Gym/Home/slide.html';
+        } else {
+            alert('اسم المستخدم أو كلمة المرور غير صحيحة.');
+        }
+    })
+    .catch(error => {
+        console.error('حدث خطأ في الاتصال بالخادم:', error);
+    });
+};
